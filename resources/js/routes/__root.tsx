83cb717a2e85f77f495717fa5a1d73a61af40501
Tawanda Munongo <ejmunongo@gmail.com>
2024-02-16 09:39:43 +0200

    PWA15 Chakra UI set up and PWA4 change to mysql

--- resources/js/routes/__root.tsx
+++ resources/js/routes/__root.tsx
@@ -1,16 +1,19 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Link as TanStackLink, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { SunIcon, Icon } from '@chakra-ui/icons';
+import { MdOutlineList } from 'react-icons/md';
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <div>
-                <Link to="/" className="[&.active]:font-bold">
-                    Home
-                </Link>{' '}
-                <Link to="/about" className="[&.active]:font-bold">
-                    About
-                </Link>
+                <ChakraLink to="/" as={TanStackLink} className="[&.active]:font-bold">
+                    <Icon as={MdOutlineList} boxSize={6} />  Home
+                </ChakraLink>{' '}
+                <ChakraLink to="/about" as={TanStackLink} className="[&.active]:font-bold">
+                    About <SunIcon boxSize={6} />
+                </ChakraLink>
             </div>
             <hr />
             <Outlet />
